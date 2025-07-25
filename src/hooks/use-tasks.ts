ba9d9e31f620c5ff9b3f6bb360fb1f311ace0d5d
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-07-25 11:50:16 -0300

    chore: implemented the delay when loading tasks

--- src/hooks/use-tasks.ts
+++ src/hooks/use-tasks.ts
@@ -1,10 +1,27 @@
+import React from "react"
 import useLocalStorage from "use-local-storage"
+import { delay } from "../helpers/utils"
 import { TASKS_KEY, TaskState, type Task } from "../models/task"
 
 
 
 export default function UseTasks() {
- const [tasks] = useLocalStorage<Task[]>(TASKS_KEY, [])
+ const [tasksData] = useLocalStorage<Task[]>(TASKS_KEY, [])
+ const [tasks, setTasks]  = React.useState<Task[]>([])
+ const [isLoadingTask, setIsLoadingTask]  = React.useState(true) 
+
+ async function fetchTasks() {
+  if(isLoadingTask) {
+    await delay(2000)
+    setIsLoadingTask(false)
+  }
+  setTasks(tasksData)
+ }
+
+
+ React.useEffect(() => {
+  fetchTasks()
+ },[tasksData])  
 
   return {
     tasks,
